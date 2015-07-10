bb05c50fddf3d611aee5177edbbdef2c5e42d686
admin <admin@mail.com>
2015-07-10 11:17:08 +0700

    add db into view

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')#mac dinh den truong ForeignKey
 	title = models.CharField(max_length=200)#ham se tu khoi tao cho bien va set type data
 	text = models.TextField()
-	create_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
 
 	def publish(self):
