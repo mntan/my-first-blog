3bb654fd6c1b95881a1ab0aaa95d4b87bf12431d
admin <admin@mail.com>
2015-07-11 12:20:25 +0700

    Added views to create/edit blog post inside the site.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,15 @@
+# from django.conf.urls import patterns, url
+# from . import views
+
+# urlpatterns = patterns('',
+#     url(r'^$', views.post_list),
+#     url(r'^post/(?P<pk>[1-9]+)/$'), views.post_detail),
+# )
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-	url(r'^$',views.post_list),
+    url(r'^$', views.post_list),    
+ 	url(r'^post/(?P<pk>[1-9]+)/$', views.post_detail),
+ 	url(r'^post/new/$', views.post_new, name='post_new'),
+ 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
